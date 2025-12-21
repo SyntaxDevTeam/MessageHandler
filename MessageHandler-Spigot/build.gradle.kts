8fb79e73ec5199d6f126cedf82a4042085931ac6
SyntaxDevTeam <wieszczy85@gmail.com>
2025-12-21 22:09:30 +0100

    [MessageHandler-Spigot] Updated dependencies

--- MessageHandler-Spigot/build.gradle.kts
+++ MessageHandler-Spigot/build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.21.11-R0.1-SNAPSHOT")
     compileOnly("com.github.ben-manes.caffeine:caffeine:3.2.3")
     compileOnly("net.kyori:adventure-text-serializer-legacy:4.26.1")
     compileOnly("net.kyori:adventure-text-minimessage:4.26.1")
